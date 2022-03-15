13145c9cb0f009bd8f2d35a52db684d1abea8307
Noamir <noamir2512@gmail.com>
2022-03-15 12:27:09 -0700

    finish q1

--- Maman11/q1/my_scalar.c
+++ Maman11/q1/my_scalar.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#define MAX_SIZE 1000 /* max size of vector */
 
-int get_scalar_product();
+int scalar_product(int size, int vec1[], int vec2[]);
 
-/* call get_scalar_product function to handle inputs, outputs and calculation of 2 vectors scalar product. Print the scalar product */
+/* get size for 2 vectors and 2 vectors integers from stdin, print the vectors scalar product */
 int main()
 {
-    int scalar_product;
-    scalar_product = get_scalar_product();
-    printf("\nThe scalar product of the vectors is: %d \n", scalar_product);
+    int size, i, scalar_product_res;
+    int vec1[MAX_SIZE];
+    int vec2[MAX_SIZE];
+
+    /* inputs: get vectors size and vectors integers from stdin */
+    printf("Enter vectors size (max valid size is %d): ", MAX_SIZE);
+    scanf("%d", &size);
+
+    printf("\nEnter %d integers for vector number 1: ", size);
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &vec1[i]);
+    }
+
+    printf("\nEnter %d integers for vector number 2: ", size);
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &vec2[i]);
+    }
+
+    /* send inputs to scalar_product function - to calculate vectors scalar product */
+    scalar_product_res = scalar_product(size, vec1, vec2);
+
+    printf("\nThe scalar product of the vectors is: %d \n", scalar_product_res);
 
     return 0;
 }
