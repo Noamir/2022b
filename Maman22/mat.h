d89ff09bc5a1d89846d1c3ec044d4ac2967a2334
Noamir <noamir2512@gmail.com>
2022-04-22 03:59:50 -0700

    first commit - working basic demo

---
+++ Maman22/mat.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+#define MAT_SIZE 4
+
+
+struct mat_def
+{
+    int size;                           /* how many rows and cols in mat (for us 4x4) */
+    float matrix[MAT_SIZE*MAT_SIZE];    /* floats array, with MAT_SIZE*MAT_SIZE places (for us 4x4) */
+};
+
+typedef struct mat_def mat;
