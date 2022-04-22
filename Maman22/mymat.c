f4ec30c17c409ae70d92b6a2617e16dc0cc57724
Noamir <noamir2512@gmail.com>
2022-04-22 08:35:16 -0700

    get command as string

--- Maman22/mymat.c
+++ Maman22/mymat.c
@@ -47,22 +47,66 @@ void read_mat(mat *m, float input[]) /* m is pointer to the actual mat address -
     for (i = 0; (i < (m->size * m->size)) && ((input[i]) != 0); i++)
     {
         m->matrix[i] = input[i];
-        printf("round num %d\n", i);
+    }
+}
+
+char *get_command()
+{
+    int limit = 10, i = 0;
+    char *tmp_ptr;
+    char *command = (char *)malloc(limit * sizeof(char));
+
+    /* menu of possible actions: */
+    printf("\nWelcome to the best matrix calculator ever! Here are your commands options:\n");
+
+    printf("\n1. read_mat <MATRIX_NAME>,<value>,<value>,<value>");
+    printf("\n2. print_mat <MATRIX_NAME>");
+    printf("\n3. add_mat <MATRIX_NAME>,<MATRIX_NAME>,<MATRIX_NAME>");
+    printf("\n4. sub_mat <MATRIX_NAME>,<MATRIX_NAME>,<MATRIX_NAME>");
+    printf("\n5. mul_mat <MATRIX_NAME>,<MATRIX_NAME>,<MATRIX_NAME>");
+    printf("\n6. mul_scalar <MATRIX_NAME>,<scaler value>,<MATRIX_NAME>");
+    printf("\n7. trans_mat <MATRIX_NAME>,<MATRIX_NAME>");
+    printf("\n8. stop - to finish the plan");
+
+    printf("\n\nEnter desired command, click Enter to execute: \n");
+
+    /* get command chars from stdin to command pointer */
+    for (i = 0; (i <= limit) && (*(command + i) = getchar()) != '\n'; i++)
+    {
+        /* got to the limit of chars size - increase limit and realloc command with new limit size */
+        if (i == limit - 1)
+        {
+            limit = 2 * limit;
+            tmp_ptr = (char *)realloc(command, limit * sizeof(char));
+
+            /* if dynamic allocation failed - return NULL */
+            if (tmp_ptr == NULL)
+            {
+                return NULL;
+            }
+
+            command = tmp_ptr;
+        }
     }
 
+    printf("\nthe command as string:\n");
+    printf("%s ", command);
+
+    return command;
 }
 
 int main()
 {
+    char *command;
 
-    mat *MAT_A = calloc(1, sizeof (mat));
-    mat *MAT_B = calloc(1, sizeof (mat));
-    mat *MAT_C = calloc(1, sizeof (mat));
-    mat *MAT_D = calloc(1, sizeof (mat));
-    mat *MAT_E = calloc(1, sizeof (mat));
-    mat *MAT_F = calloc(1, sizeof (mat));
+    mat *MAT_A = calloc(1, sizeof(mat));
+    mat *MAT_B = calloc(1, sizeof(mat));
+    mat *MAT_C = calloc(1, sizeof(mat));
+    mat *MAT_D = calloc(1, sizeof(mat));
+    mat *MAT_E = calloc(1, sizeof(mat));
+    mat *MAT_F = calloc(1, sizeof(mat));
 
-    /* init all mats cells with 0  using init_mat */
+    /* init default mats cells with 0 */
     init_mat(MAT_A);
     init_mat(MAT_B);
     init_mat(MAT_C);
@@ -70,12 +114,22 @@ int main()
     init_mat(MAT_E);
     init_mat(MAT_F);
 
+    /* get command for matrix action from stdin */
+    command = get_command();
+
+    /* if dynamic allocation failed - return NULL */
+    if (command == NULL)
+    {
+        printf("Fatal error: memory allocation failed!\n");
+        return 1;
+    }
+
     /* change matrix using read_mat --> insert 0 to end input */
     float input_a[5] = {1, 2, 3, 4, 0};
 
     read_mat(MAT_A, input_a);
 
-    float input_b[21] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,0};
+    float input_b[21] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 0};
     read_mat(MAT_B, input_b);
 
     float input_c[14] = {9000000, -80, 3, 42937892, 5.4545, 6454.54523, 7000, 1, 2, 3, 4, 5, 6, 0};
@@ -84,8 +138,8 @@ int main()
     float input_d[17] = {12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 0};
     read_mat(MAT_D, input_d);
 
-    float input_e[17] = {-1, -12, -123, -1234, -12345, 1.2345, 12.345, 123.45, 1234.5, 12345, 12345, 12345, 12345, 12345, 12345, 12345,0};
-    read_mat(MAT_E, input_e); 
+    float input_e[17] = {-1, -12, -123, -1234, -12345, 1.2345, 12.345, 123.45, 1234.5, 12345, 12345, 12345, 12345, 12345, 12345, 12345, 0};
+    read_mat(MAT_E, input_e);
 
     /* print matrix using print_mat */
     print_mat(MAT_A);
