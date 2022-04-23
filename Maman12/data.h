21613d53cd5a2f818776d6b18933338efa800aa6
Noamir <noamir2512@gmail.com>
2022-04-23 07:26:46 -0700

    int array to char array change

--- Maman12/data.h
+++ Maman12/data.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-#define DEFAULT_LIMIT 50    /* default limit of array size, if limit is reached - realloc will be made */
+#define DEFAULT_LIMIT 50    /* default limit of chars to get from stdin. if limit is reached - realloc will be made */
 #define LINE_LEN 20         /* number of chars in each printed line */
 #define INCREAS_BY 2        /* when limit is reached - increase it by this number */
 
