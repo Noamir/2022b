1c6f47bc211f38b6767f98c8e969557913effc73
Noamir <noamir2512@gmail.com>
2022-06-01 08:03:29 -0700

    a lot of changes

--- Maman23/lists_c/lists_c.c
+++ Maman23/lists_c/lists_c.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define FILE_PATH_LEN 2048 /* default limit of file path chars length, to get from argv */
+#define FILE_PATH_LEN 2048 /* default length of file path chars length, to get from argv */
 #define NODE_CHARS 10      /* number of chars in each list node */
 #define LINE_LEN 20        /* number of chars in each printed line */
 
--- Maman23/my_and/my_and.c
+++ Maman23/my_and/my_and.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define ALL_BITS_ON 4294967295
 
 /** get_bits_and:
 Gets count parameter which indicates how many unsigned int parameters this function will get.
@@ -13,81 +12,128 @@ int get_bits_and(unsigned int count, ...)
 {
     va_list args;
     int i;
-    unsigned int curr, bits_and = ALL_BITS_ON; /* initial bits_and value is the max unsigned int value - so it won't affect the result */
-
+    unsigned int curr, bits_and = ~0;       /* initial bits_and value is the value of all on bits - so it won't affect the result */
     va_start(args, count);
     for (i = 0; i < count; i++)
     {
-        curr = va_arg(args, unsigned int); /* get the current parameter */
-        bits_and = bits_and & curr;        /* calculate the & result with the current parameter */
+        curr = va_arg(args, unsigned int);  /* get the current parameter */
+        bits_and = bits_and & curr;         /* calculate the & result with the current parameter */
     }
 
     va_end(args);
     return bits_and;
 }
 
-
-/** print_parameters:
+/** print_decimal:
 Gets count parameter which indicates how many unsigned int parameters this function will get.
 The function can get dynamic unlimited unsigned int parameters.
-The function prints all parameters, seperated by commas. (exclusing the count parameter)
+The function prints all parameters in decimal base, seperated by commas. (exclusing the count parameter)
  **/
-void print_parameters(unsigned int count, ...)
+void print_decimal(unsigned int count, ...)
 {
     va_list args;
     int i;
     unsigned int curr;
 
     va_start(args, count);
-    printf("The parameters are:\n");
+    printf("The parameters in Decimal base:\n");
     for (i = 0; i < count; i++)
     {
-        curr = va_arg(args, unsigned int); /* get the current parameter */
+        curr = va_arg(args, unsigned int);  /* get the current parameter */
         if (i == count - 1)
-            printf("%d", curr); /* print the current parameter without a following comma */
+            printf("%u", curr);             /* print the current parameter without a following comma */
         else
-            printf("%d, ", curr); /* print the current parameter with a following comma */
+            printf("%u, ", curr);           /* print the current parameter with a following comma */
     }
-    printf("\n");
+    
+    printf("\n\n");
     va_end(args);
 }
 
+/** print_hexadecimal:
+Gets count parameter which indicates how many unsigned int parameters this function will get.
+The function can get dynamic unlimited unsigned int parameters.
+The function prints all parameters in hexadecimal base, seperated by commas. (exclusing the count parameter)
+ **/
+void print_hexadecimal(unsigned int count, ...)
+{
+    va_list args;
+    int i;
+    unsigned int curr;
+
+    va_start(args, count);
+    printf("The parameters in Hexadecimal base:\n");
+    for (i = 0; i < count; i++)
+    {
+        curr = va_arg(args, unsigned int);  /* get the current parameter */
+        if (i == count - 1)
+            printf("%X", curr);             /* print the current parameter without a following comma */
+        else
+            printf("%X, ", curr);           /* print the current parameter with a following comma */
+    }
+    
+    printf("\n\n");
+    va_end(args);
+}
 
 /** This program calls get_bits_and function 4 times.
  The program prints to stdout:
- 1. All parameters sent to get_bits_and function - dynamic unlimited number of parameters
- 2. bits_and_result in Decimal representation
- 3. bits_and_result in Hexadecimal representation
+ 1. All parameters sent to get_bits_and function - dynamic unlimited number of parameters - in Decimal base
+ 2. All parameters sent to get_bits_and function - dynamic unlimited number of parameters - in Hexadecimal base
+ 2. bits_and_result in Decimal base
+ 3. bits_and_result in Hexadecimal base
 **/
 int main(void)
 {
     unsigned int bits_and_result;
 
     printf("======== RUN 1 ========\n\n");
-    print_parameters(3, 5, 7, 61);
+    print_decimal(3, 5, 7, 61);
+    print_hexadecimal(3, 5, 7, 61);
     bits_and_result = get_bits_and(3, 5, 7, 61);
-    printf("bits_and_result Decimal: %d\n", bits_and_result);
+    printf("bits_and_result Decimal: %u\n", bits_and_result);
     printf("bits_and_result Hexadecimal: %X\n", bits_and_result);
     printf("\n=======================\n\n");
 
     printf("======== RUN 2 ========\n\n");
-    print_parameters(5, 15, 687, 831, 349535, 95);
+    print_decimal(5, 15, 687, 831, 349535, 95);
+    print_hexadecimal(5, 15, 687, 831, 349535, 95);
     bits_and_result = get_bits_and(5, 15, 687, 831, 349535, 95);
-    printf("bits_and_result Decimal: %d\n", bits_and_result);
+    printf("bits_and_result Decimal: %u\n", bits_and_result);
     printf("bits_and_result Hexadecimal: %X\n", bits_and_result);
     printf("\n=======================\n\n");
 
     printf("======== RUN 3 ========\n\n");
-    print_parameters(10, 938, 106, 170, 362, 746, 3818, 2794, 2730, 31402, 55978);
+    print_decimal(10, 938, 106, 170, 362, 746, 3818, 2794, 2730, 31402, 55978);
+    print_hexadecimal(10, 938, 106, 170, 362, 746, 3818, 2794, 2730, 31402, 55978);
     bits_and_result = get_bits_and(10, 938, 106, 170, 362, 746, 3818, 2794, 2730, 31402, 55978);
-    printf("bits_and_result Decimal: %d\n", bits_and_result);
+    printf("bits_and_result Decimal: %u\n", bits_and_result);
     printf("bits_and_result Hexadecimal: %X\n", bits_and_result);
     printf("\n=======================\n\n");
 
     printf("======== RUN 4 ========\n\n");
-    print_parameters(15, 262143, 8388607, 8126463, 6029291, 3932137, 2359264, 3407840, 3932128, 8126432, 8126433, 6029283, 5898179, 5799683, 5930771, 5996288);
+    print_decimal(15, 262143, 8388607, 8126463, 6029291, 3932137, 2359264, 3407840, 3932128, 8126432, 8126433, 6029283, 5898179, 5799683, 5930771, 5996288);
+    print_hexadecimal(15, 262143, 8388607, 8126463, 6029291, 3932137, 2359264, 3407840, 3932128, 8126432, 8126433, 6029283, 5898179, 5799683, 5930771, 5996288);
     bits_and_result = get_bits_and(15, 262143, 8388607, 8126463, 6029291, 3932137, 2359264, 3407840, 3932128, 8126432, 8126433, 6029283, 5898179, 5799683, 5930771, 5996288);
-    printf("bits_and_result Decimal: %d\n", bits_and_result);
+    printf("bits_and_result Decimal: %u\n", bits_and_result);
+    printf("bits_and_result Hexadecimal: %X\n", bits_and_result);
+    printf("\n=======================\n\n");
+
+
+    /* Extra test runs */
+    printf("======== RUN 5 ========\n\n");
+    print_decimal(0);
+    print_hexadecimal(0);
+    bits_and_result = get_bits_and(0);
+    printf("bits_and_result Decimal: %u\n", bits_and_result);
+    printf("bits_and_result Hexadecimal: %X\n", bits_and_result);
+    printf("\n=======================\n\n");
+
+    printf("======== RUN 6 ========\n\n");
+    print_decimal(3, 2, 6, 10);
+    print_hexadecimal(3, 2, 6, 10);
+    bits_and_result = get_bits_and(3, 2, 6, 10);
+    printf("bits_and_result Decimal: %u\n", bits_and_result);
     printf("bits_and_result Hexadecimal: %X\n", bits_and_result);
     printf("\n=======================\n\n");
 
